b1e5e5a75a5f94256e1237cd13365b6917222e2f
Cerino O. Ligutom III <zeferinix@gmail.com>
2017-05-28 09:04:49 +0800

    Refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LandingPageComponent } from './landing-page/index';
 import { AppRoutingModule } from './app-routing.module';
+import { LandingPageComponent } from './landing-page/index';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
-import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+
+import { DashboardModule } from './dashboard/dashboard.module';
+
 
 @NgModule({
   declarations: [
@@ -19,6 +22,7 @@ import { NotFoundPageComponent } from './not-found-page/not-found-page.component
     BrowserModule,
     CoreModule,
     SharedModule,
+    DashboardModule,
     AppRoutingModule
   ],
   providers: [],
